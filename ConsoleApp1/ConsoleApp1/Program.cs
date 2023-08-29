4b2f66b5fd339515dab72b3e72554e9595caa605
Predator <murad.mammedzade11@gmail.com>
2023-08-29 04:27:22 -0700

    save

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Taskü
+{
+    internal class Program
+    {
+        public static bool CustomTryParse(string a, out int b)
+        {
+            b = 0;
+            try
+            {
+                b = int.Parse(a);
+                return true;
+
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string a = "100";
+            int b;
+            if (CustomTryParse(a, out b))
+            { Console.WriteLine(b); }
+            else
+            { Console.WriteLine(b); }
+
+
+
+        }
+    }
+
+}
